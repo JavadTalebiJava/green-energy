9cd983f31adb9eba00abb9100d3ba5de496bf482
JavadTalebi <cevat.talebi@gmail.com>
2020-02-16 18:08:11 +0300

    Add Entities

--- src/main/java/com/greenenergy/GreenenergyApplication.java
+++ src/main/java/com/greenenergy/GreenenergyApplication.java
@@ -1,4 +1,4 @@
-package com.greenenergy.greenenergy;
+package com.greenenergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/greenenergy/model/InitialProcessStatus.java
@@ -0,0 +1,10 @@
+package com.greenenergy.model;
+
+/**
+ * Available statuses for initial process.
+ */
+public enum InitialProcessStatus {
+    STARTED,
+    FINISHED,
+    FAILED
+}
